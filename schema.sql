eb530f3b53f13a2b65d277c8988d8826ecc10588
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-21 12:48:18 +0200

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY, 
+    user_id INTEGER REFERENCES users, 
+    title TEXT, 
+    description TEXT, 
+    date TIMESTAMP, 
+    due_date TEXT, 
+    priority INTEGER, 
+    status TEXT
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY, 
+    user_id INTEGER REFERENCES users, 
+    name TEXT
+);
