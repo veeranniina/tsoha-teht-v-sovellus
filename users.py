3e301ba2023fc8fba874b51037f2521809da167d
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-22 14:05:42 +0200

    error correction

---
+++ users.py
@@ -0,0 +1,42 @@
+import os
+from db import db
+from flask import abort, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def login(name, password):
+    sql = "SELECT password, id, role FROM users WHERE name=:name"
+    result = db.session.execute(sql, {"name":name})
+    user = result.fetchone()
+    if not user:
+        return False
+    if not check_password_hash(user[0], password):
+        return False
+    session["user_id"] = user[1]
+    session["user_name"] = name
+    session["user_role"] = user[2]
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
+
+def logout():
+    del session["user_id"]
+
+def register(username, password):    #role???
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (name, password, role) VALUES (:username, :password)" #role???
+        db.session.execute(sql, {"name":username, "password":hash_value}) #"role":role
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def user_id():
+    return session.get("user_id", 0)
+
+def require_role(role):
+    if role > session.get("user_role", 0):
+        abort(403)
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
