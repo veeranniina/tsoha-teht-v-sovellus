0f26db8bd61950867e10ae5cd432003f42143c67
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-26 15:18:03 +0200

    fixed python code

--- users.py
+++ users.py
@@ -1,7 +1,7 @@
-from db import db
 from flask import abort, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import text
+from db import db
 
 def login(username, password):
     sql = text("SELECT id, password FROM users WHERE username=:username")   #???toimiiko
