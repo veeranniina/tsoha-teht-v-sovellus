17042a7a827028a15001b3f35e0e5c50d30fae3c
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-26 13:31:12 +0200

    tried to fix register problem

--- users.py
+++ users.py
@@ -1,21 +1,20 @@
-import os
 from db import db
 from flask import abort, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy import text
 
-def login(name, password):
-    sql = "SELECT password, id, role FROM users WHERE name=:name"
-    result = db.session.execute(sql, {"name":name})
+def login(username, password):
+    sql = text("SELECT id, password FROM users WHERE username=:username")   #???toimiiko
+    result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
         return False
-    if not check_password_hash(user[0], password):
-        return False
-    session["user_id"] = user[1]
-    session["user_name"] = name
-    session["user_role"] = user[2]
-    session["csrf_token"] = os.urandom(16).hex()
-    return True
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
 
 def logout():
     del session["user_id"]
@@ -23,8 +22,8 @@ def logout():
 def register(username, password):    #role???
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (name, password, role) VALUES (:username, :password)" #role???
-        db.session.execute(sql, {"name":username, "password":hash_value}) #"role":role
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)" #role???
+        db.session.execute(sql, {"username":username, "password":hash_value}) #"role":role
         db.session.commit()
     except:
         return False
@@ -33,9 +32,9 @@ def register(username, password):    #role???
 def user_id():
     return session.get("user_id", 0)
 
-def require_role(role):
-    if role > session.get("user_role", 0):
-        abort(403)
+#def require_role(role):
+    #if role > session.get("user_role", 0):
+        #abort(403)
 
 def check_csrf():
     if session["csrf_token"] != request.form["csrf_token"]:
