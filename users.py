67ae9ab493a9d68fd4b925656c9f164f8192b205
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-27 10:43:02 +0200

    tried to fix register!

--- users.py
+++ users.py
@@ -1,7 +1,8 @@
-from flask import abort, request, session
+from db import db
+from flask import session, request, abort
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.sql import text
-from db import db
+
 
 def login(username, password):
     sql = text("SELECT id, password FROM users WHERE username=:username")  #???toimiiko
