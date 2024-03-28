3dd7e86dbaec4f82e9a89e239c700186bcb2922f
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-03-28 12:59:29 +0200

    fixed function login

--- users.py
+++ users.py
@@ -13,6 +13,7 @@ def login(username, password):
     else:
         if check_password_hash(user.password, password):
             session["user_id"] = user.id
+            session["username"] = username
             return True
         else:
             return False
