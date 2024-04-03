b3c4e5b8f337018320b9c46b80e7bbd620903143
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-04-03 15:41:50 +0300

    tried to fix note deletion

--- tasks.py
+++ tasks.py
@@ -10,28 +10,28 @@ def get_task_list():
     result = db.session.execute(sql, {"user_id": user_id})
     return result.fetchall()
 
-def create_task(title, description, due_date, priority, status):
+def create_task(title, description, date, due_date, priority):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = text("INSERT INTO tasks (user_id, title, description, due_date, priority, status) VALUES (:user_id, :title, :description, :due_date, :priority, :status)")
-    db.session.execute(sql, {"user_id": user_id, "title": title, "description": description, "due_date": due_date, "priority": priority, "status": status})
+    sql = text("INSERT INTO tasks (user_id, title, description, date, due_date, priority) VALUES (:user_id, :title, :description, :date, :due_date, :priority)")
+    db.session.execute(sql, {"user_id": user_id, "title": title, "description": description, "date": date, "due_date": due_date, "priority": priority})
     db.session.commit()
     return True
 
-def update_task(task_id, title, description, due_date, priority, status):
+def update_task(task_id, title, description, date, due_date, priority):
     user_id = users.user_id()
     if user_id == 0:
         return False
     try: #jos arvo on 'None', käytetään tehtävän nykyistä arvoa tietokannassa
-        sql = text("UPDATE tasks SET title = COALESCE(:title, title), description = COALESCE(:description, description), due_date = COALESCE(:due_date, due_date), priority = COALESCE(:priority, priority), status = COALESCE(:status, status) WHERE id = :task_id AND user_id = :user_id")
-        db.session.execute(sql, {"title": title, "description": description, "due_date": due_date, "priority": priority, "status": status, "task_id": task_id, "user_id": user_id})
+        sql = text("UPDATE tasks SET title = COALESCE(:title, title), description = COALESCE(:description, description), due_date = COALESCE(:due_date, due_date), priority = COALESCE(:priority, priority) WHERE id = :task_id AND user_id = :user_id")
+        db.session.execute(sql, {"title": title, "description": description, "date": date, "due_date": due_date, "priority": priority, "task_id": task_id, "user_id": user_id})
         db.session.commit()
         return True
     except:
         return False
 
 def delete_task(task_id, user_id):
-    sql = text("UPDATE tasks SET visible=0 WHERE id=:task_id and user_id=:user_id")  
-    db.session.execute(sql, {"task_id":task_id, "user_id":user_id})
+    sql = text("DELETE FROM tasks WHERE id=:task_id AND user_id=:user_id")
+    db.session.execute(sql, {"task_id": task_id, "user_id": user_id})
     db.session.commit()
