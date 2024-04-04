1628aae3155df7b1c4cb7761c2a65d25095f2350
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-04-04 13:21:37 +0300

    created def generate_csrf_token and before_request()

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from os import getenv
-from flask import Flask
+from flask import Flask, session
+import secrets
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+
+def generate_csrf_token():
+    session["csrf_token"] = secrets.token_hex(16)
+
+#ennen pyyntöä tarkastetaan, onko CSRF-tokeni tallennettu istuntoon
+@app.before_request
+def before_request():
+    if "csrf_token" not in session:
+        generate_csrf_token()
 
 import routes
