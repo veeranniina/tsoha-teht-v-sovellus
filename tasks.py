ac97b87640ed497e0e66aecc40dfd8ebf4701214
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-04-16 10:49:30 +0300

    added to database queries category_id

--- tasks.py
+++ tasks.py
@@ -9,22 +9,22 @@ def get_task_list(user_id):
     result = db.session.execute(sql, {"user_id": user_id})
     return result.fetchall()
 
-def create_task(title, description, date, due_date, priority):
+def create_task(title, description, date, due_date, priority, category_id):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = text("INSERT INTO tasks (user_id, title, description, date, due_date, priority) VALUES (:user_id, :title, :description, :date, :due_date, :priority)")
-    db.session.execute(sql, {"user_id": user_id, "title": title, "description": description, "date": date, "due_date": due_date, "priority": priority})
+    sql = text("INSERT INTO tasks (user_id, title, description, date, due_date, priority, category_id) VALUES (:user_id, :title, :description, :date, :due_date, :priority, :category_id)")
+    db.session.execute(sql, {"user_id": user_id, "title": title, "description": description, "date": date, "due_date": due_date, "priority": priority, "category_id": category_id})
     db.session.commit()
     return True
 
-def edit_task(task_id, title, description, date, due_date, priority):
+def edit_task(task_id, title, description, date, due_date, priority, category_id):
     user_id = users.user_id()
     if user_id == 0:
         return False
     try: #jos arvo on 'None', käytetään tehtävän nykyistä arvoa tietokannassa
-        sql = text("UPDATE tasks SET title = COALESCE(:title, title), description = COALESCE(:description, description), due_date = COALESCE(:due_date, due_date), priority = COALESCE(:priority, priority) WHERE id = :task_id AND user_id = :user_id")
-        db.session.execute(sql, {"title": title, "description": description, "date": date, "due_date": due_date, "priority": priority, "task_id": task_id, "user_id": user_id})
+        sql = text("UPDATE tasks SET title = COALESCE(:title, title), description = COALESCE(:description, description), due_date = COALESCE(:due_date, due_date), priority = COALESCE(:priority, priority), category_id = :category_id WHERE id = :task_id AND user_id = :user_id")
+        db.session.execute(sql, {"title": title, "description": description, "date": date, "due_date": due_date, "priority": priority, "category_id": category_id, "task_id": task_id, "user_id": user_id})
         db.session.commit()
         return True
     except:
