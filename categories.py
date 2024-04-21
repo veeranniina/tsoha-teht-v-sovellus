0d9cc6f1c95d0f2c163b940aeae3f81792151ea4
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-04-21 15:40:14 +0300

    added new feature where category name must be unique

--- categories.py
+++ categories.py
@@ -1,14 +1,23 @@
 from db import db
 from sqlalchemy.sql import text
 import users
+from flask import flash
 
 def create_category(name):
     user_id = users.user_id()
     if user_id == 0:
         return False
     try:
-        sql = text("INSERT INTO categories (user_id, name) VALUES (:user_id, :name)")
-        db.session.execute(sql, {"user_id": user_id, "name": name})
+        #onko olemassa samanniminen
+        sql_check = text("SELECT id FROM categories WHERE user_id = :user_id AND name = :name")
+        result = db.session.execute(sql_check, {"user_id": user_id, "name": name})
+        existing_category = result.fetchone()
+        if existing_category:
+            return False  
+
+        #jos ei, lisätään db
+        sql_insert = text("INSERT INTO categories (user_id, name) VALUES (:user_id, :name)")
+        db.session.execute(sql_insert, {"user_id": user_id, "name": name})
         db.session.commit()
         return True
     except:
@@ -26,8 +35,31 @@ def edit_category(id, name):
     except:
         return False
     
-def get_categories_from_database():
-    sql = text("SELECT id, user_id, name FROM categories")  #muokkaa tätä
-    result = db.session.execute(sql)
+def get_categories_from_database(user_id):
+    sql = text("SELECT id, user_id, name FROM categories WHERE user_id = :user_id")
+    result = db.session.execute(sql, {"user_id": user_id})
     categories = result.fetchall()
     return categories
+
+def create_category(name):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    try:
+        #tarkistetaan että poistettava kategoria kuuluu käyttäjälle
+        sql_check = text("SELECT id FROM categories WHERE user_id = :user_id AND name = :name")
+        result = db.session.execute(sql_check, {"user_id": user_id, "name": name})
+        existing_category = result.fetchone()
+        if existing_category:
+            flash("Samanniminen kategoria on jo olemassa!", "error")
+            return False  
+        
+        #lisätään tietokantaan
+        sql_insert = text("INSERT INTO categories (user_id, name) VALUES (:user_id, :name)")
+        db.session.execute(sql_insert, {"user_id": user_id, "name": name})
+        db.session.commit()
+        flash("Kategoria lisättiin onnistuneesti!", "success")
+        return True
+    except:
+        flash("Kategorian lisäys epäonnistui!", "error")
+        return False
