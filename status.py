85f23d5b47b61b59f29a685d10f114cc925f1080
Veera Viirumäki <veera.viirumaki@gmail.com>
2024-04-24 18:41:40 +0300

    commented function delete_status

--- status.py
+++ status.py
@@ -2,28 +2,30 @@ from db import db
 from sqlalchemy.sql import text
 import users
 
-def create_status(user_id, name):
-    user_id = users.user_id()
-    if user_id == 0:
-        return False
-    sql = text("INSERT INTO status (user_id, name) VALUES (:user_id, :name)")
-    db.session.execute(sql, {"user_id": user_id, "name": name})
-    db.session.commit()
-    return True
+def get_statuses():
+    sql = text("SELECT id, name FROM status")
+    result = db.session.execute(sql)
+    return [{"id": row[0], "name": row[1]} for row in result]
 
 def edit_status(status_id, name):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    try: #jos arvo on 'None', käytetään tehtävän nykyistä arvoa tietokannassa
+    try: 
         sql = text("UPDATE status SET name = :name WHERE id = :status_id")
         db.session.execute(sql, {"status_id": status_id, "name": name})
         db.session.commit()
         return True
-    except:
+    except Exception as e:
+        print(f"Error editing status: {e}")
         return False
     
-def delete_status(status_id, user_id):
-    sql = text("DELETE FROM status WHERE id=:status_id AND user_id=:user_id")
-    db.session.execute(sql, {"status_id": status_id, "user_id": user_id})
-    db.session.commit()
+#def delete_status(status_id, user_id):
+ #   try:
+  #      sql = text("DELETE FROM status WHERE id=:status_id AND user_id=:user_id")
+   #     db.session.execute(sql, {"status_id": status_id, "user_id": user_id})
+    #    db.session.commit()
+     #   return True
+    #except Exception as e:
+     #   print(f"Error deleting status: {e}")
+      #  return False
